6a39a1aa93fa7997d9393fea4d091af681159f61
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-09-10 21:02:33 +0530

    feat: add terraform infrastructure and k8s manifest

---
+++ infrastructure/eks.tf
@@ -0,0 +1,50 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "ecommerce-gitops-cluster"
+  cluster_version = "1.34"
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    general = {
+      name         = "general-nodes"
+      min_size     = 1
+      max_size     = 4
+      desired_size = 3
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+
+      ami_type = "AL2023_x86_64_STANDARD"
+
+      # AL2023 native YAML config to unlock 28 pods per node
+      cloudinit_pre_nodeadm = [
+        {
+          content_type = "application/node.eks.aws"
+          content      = <<-EOT
+          ---
+          apiVersion: node.eks.aws/v1alpha1
+          kind: NodeConfig
+          spec:
+            kubelet:
+              config:
+                maxPods: 28
+        EOT
+        }
+      ]
+    }
+  }
+  tags = {
+    Environment = "dev"
+    Project     = "gitops-ecommerce"
+  }
+}
---
+++ infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# The Network Foundation: Using the official AWS VPC Module (our package manager approach)
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "ecommerce-gitops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
